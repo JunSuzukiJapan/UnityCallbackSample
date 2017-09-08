251e004df16940f84caed93192b3145911319435
Jun Suzuki <jun.suzuki.japan@gmail.com>
2017-09-08 17:07:43 +0900

    create UI

--- Assets/Scripts/Caller.cs
+++ Assets/Scripts/Caller.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Caller : MonoBehaviour {
-	void Start () {
-		SampleCallback.CallPlugin(8, OnEndOfCalculate);
+	public InputField input;
+	public Text text;
+	public void Multiply(){
+		string s = input.text;
+		int result;
+		if(int.TryParse(s, out result)){
+			SampleCallback.CallPlugin(result, OnEndOfCalculate);
+		}else{
+			Debug.LogFormat("can't parse: {0}", s);
+		}
 	}
 
 	void OnEndOfCalculate(int x){
 		Debug.LogFormat("x: {0}", x);
+		text.text = string.Format("{0}", x);
 	}
 }
